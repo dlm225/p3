dd06b4605d2e6e1c1a47318ac4b86b42dbb938df
David Morgan <dlm225@g.harvard.edu>
2016-03-14 12:03:16 -0400

    added sample route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('welcome');});
+Route::get('/temp', function() {return 'Temp page successful!';});
 
 /*
 |--------------------------------------------------------------------------
