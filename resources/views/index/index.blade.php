08caa67b2d24bad422800d84fcfb508f39e0d11e
David Morgan <dlm225@g.harvard.edu>
2016-03-23 21:42:31 -0400

    centered links on index

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.master')
 
 @section('title')
-    Index
+    Dev's Best Friend - Index
 @stop
 
 @section('content')
-<nav>
+<nav class="text-center">
     <ul>
         <li><a href='/text'>Generate Lorem Ipsum</a></li>
         <li><a href='/user'>Generate Random User</a></li>
