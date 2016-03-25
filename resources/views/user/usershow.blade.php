7b27b2399c0b5f4c22fc00e15b7ff7d321a33425
David Morgan <dlm225@g.harvard.edu>
2016-03-25 15:31:29 -0400

    added photos and css

--- resources/views/user/usershow.blade.php
+++ resources/views/user/usershow.blade.php
@@ -11,9 +11,26 @@
     <button onclick="goBack()" class='btn btn-primary block-center'>Go Back</button><br />
 </div>
 <br />
-<div class="col-md-8 col-md-offset-2">
+<div class="container">
     <p>
-
+        @for ($i = 0; $i < 10; $i++)
+            <div class="fancybox">
+                <?php
+                    require_once '../vendor/fzaninotto/faker/src/autoload.php';
+                    $faker = Faker\Factory::create('en_US');
+                    echo '<table><tr><td>';
+                    echo '<h2>'.$faker->name.'</h2><br />';
+                    echo $faker->streetAddress.'<br />';
+                    echo $faker->city.', '.$faker->stateAbbr.' '.$faker->postcode.'<br />';
+                    echo $faker->phoneNumber.'<br />';
+                    echo $faker->email.'<br />';
+                    echo $faker->company.'<br /></td>';
+                    echo '<td><img class="profilepic" src='.$faker->imageUrl($width = 640, $height = 480).'></td><tr></table>' // 'http://lorempixel.com/640/480/'
+                ?>
+            </div>
+            <br />
+        @endfor
+    </p>
 </div>
 
 @stop
