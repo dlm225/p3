d357633085c51819792b4023ce3160b321540c47
David Morgan <dlm225@g.harvard.edu>
2016-03-26 13:39:15 -0400

    updated credits

--- resources/views/main/about.blade.php
+++ resources/views/main/about.blade.php
@@ -8,8 +8,10 @@
 <div class="container">
     Credits:
     <ol>
+        <li> Website constructed with Larvel: https://laravel.com/</li>
         <li> Lorem Ipsum Generator: "Lorem-Ipsum" (badcow/lorem-ipsum) @ https://packagist.org/packages/badcow/lorem-ipsum</li>
         <li> Persona Generator: "Faker" (fzaninotto/faker) @ https://packagist.org/packages/fzaninotto/faker</li>
+        <li> Website Logo: Flaticon @ http://www.flaticon.com/free-icons/worker_22204</li>
     </ol>
 </div>
 @stop
